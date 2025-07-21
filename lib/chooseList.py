ee6baf3bd8b56141d35d2a033072c2052d98fdf8
KsmBl <katzen.sind.lecker69@gmail.com>
2025-07-21 13:50:51 +0200

    enable number keys in chooseList

--- lib/chooseList.py
+++ lib/chooseList.py
@@ -26,5 +26,11 @@ def chooseList(inputList):
 			selectEntry = min(len(inputList) - 1, selectEntry + 1)
 		elif key == "enter":
 			return inputList[selectEntry]
+		elif key in ["1", "2", "3", "4", "5", "6", "7", "8", "9"]:
+			if int(key) <= len(inputList):
+				return inputList[int(key) - 1]
+		elif key == "0":
+			if len(inputList) >= 10:
+				return inputList[9]
 		elif key == "q":
 			return None
