503ebfdb474bfcb998b8d953ecb40097f9b1a631
KsmBl <katzen.sind.lecker69@gmail.com>
2025-08-08 11:57:32 +0200

    google calender get highlights for current month

--- lib/getGoogleCaledar.py
+++ lib/getGoogleCaledar.py
@@ -1,13 +1,19 @@
 from gcsa.google_calendar import GoogleCalendar
 from datetime import datetime, timedelta
+import calendar
 
-calendar = GoogleCalendar("katzen.sind.lecker69@gmail.com")
+gc = GoogleCalendar("katzen.sind.lecker69@gmail.com")
 
-startDate = datetime.now()
-endDate = startDate + timedelta(days=7)
+now = datetime.now()
 
-events = calendar.get_events(time_min=startDate, time_max=endDate)
+start_of_month = now.replace(day=1, hour=0, minute=0, second=0, microsecond=0)
+lastDay = calendar.monthrange(now.year, now.month)[1]
+end_of_month = now.replace(day=lastDay, hour=23, minute=59, second=59, microsecond=999999)
 
-print(f"📆 Termine vom {startDate.date()} bis {endDate.date()}:\n")
+events = gc.get_events(time_min=start_of_month, time_max=end_of_month)
+
 for event in events:
-	print(f"- {event.start.strftime('%A, %d.%m.%Y %H:%M')} – {event.summary}")
+	status = event.other.get('status', 'confirmed')
+	if status != 'cancelled':
+		start = event.start.strftime('%A, %d.%m.%Y %H:%M') if isinstance(event.start, datetime) else event.start.strftime('%A, %d.%m.%Y')
+		print(f"{start} – {event.summary}")
