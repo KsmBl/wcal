ac8e1c41d1fff999da9a69f04058243fe4c4ff0e
KsmBl <katzen.sind.lecker69@gmail.com>
2025-08-11 11:52:06 +0200

    editHighlights refactor

--- lib/editDayHighlights.py
+++ lib/editDayHighlights.py
@@ -19,11 +19,8 @@ def editDayHighlights(day, month, year):
 	while True:
 		SAVE_DIRECTORY = os.path.expanduser(getConfig("highlightSaveDirectory"))
 		highlightPath = f"{year}/{month}.json"
-		if not os.path.exists(os.path.join(SAVE_DIRECTORY, str(year))):
-			os.makedirs(os.path.join(SAVE_DIRECTORY, str(year)))
 
-		if not os.path.isfile(os.path.join(SAVE_DIRECTORY, highlightPath)):
-			writeJson({}, highlightPath)
+		createMissingPathObjects(SAVE_DIRECTORY, year, highlightPath)
 
 		highlights = readJson(highlightPath)
 		dayEntries = []
@@ -46,30 +43,15 @@ def editDayHighlights(day, month, year):
 			pickedEntry = pickedEntry.split(" ")[0]
 
 			if pickedID <= len(arrayDayEntries) - 3:
-				edit = askQuestion("edit this Entry:", ["edit", "delete", "return"])
-				_pickedEntry = re.sub(r'\x1b\[[0-9;]*m', '', pickedEntry)
 
-				if edit == 0:
-					editEntry(_pickedEntry, highlights, day, highlightPath)
-				elif edit == 1:
-					rt = askQuestion("delete entry?", ["no", "yes"])
-					if rt == 1:
-						rt2 = deleteEntry(_pickedEntry, highlights, day, highlightPath)
-						if rt2 == "reloadDay":
-							return "reloadDay"
+				editEntryMenu(pickedEntry, highlights, day, highlightPath)
+
 			else:
 				# new Entry
 				if pickedID == len(arrayDayEntries) - 2:
-					_hour, _minute = getTime()
-					_name = getString()
-					_color = getColor()
+					if newEntry(highlights, day, highlightPath) == "reloadDay":
+						return "reloadDay"
 
-					highlights[str(day)].setdefault(f"{_hour}:{_minute}", {})
-
-					highlights[str(day)][f"{_hour}:{_minute}"]["name"] = _name
-					highlights[str(day)][f"{_hour}:{_minute}"]["color"] = _color
-
-					writeJson(highlights, highlightPath)
 				# return
 				elif pickedID == len(arrayDayEntries) - 1:
 					if changedDayVisuality == 0:
@@ -78,15 +60,50 @@ def editDayHighlights(day, month, year):
 						return "reloadDay"
 
 		else:
-			_hour, _minute = getTime()
-			_name = getString()
-			_color = getColor()
-			highlights[str(day)] = {
-				f"{_hour}:{_minute}" : {
-					"name" : _name,
-					"color" : _color
-				}
-			}
+			highlights[str(day)] = {}
+			newEntry(highlights, day, highlightPath)
 
-			writeJson(highlights, highlightPath)
 			changedDayVisuality = 1
+
+
+def editEntryMenu(pickedEntry, highlights, day, highlightPath):
+	# remove color code
+	_pickedEntry = re.sub(r'\x1b\[[0-9;]*m', '', pickedEntry)
+
+	# index of choosen option
+	edit = askQuestion("edit this Entry:", ["edit", "delete", "return"])
+
+	if edit == 0:
+		editEntry(_pickedEntry, highlights, day, highlightPath)
+	elif edit == 1:
+		if askQuestion("delete entry?", ["no", "yes"]) == 1:
+			if deleteEntry(_pickedEntry, highlights, day, highlightPath) == "reloadDay":
+				return "reloadDay"
+
+
+def newEntry(highlights, day, highlightPath):
+	_hour, _minute, _name, _color = getEntryData()
+	highlights[str(day)].setdefault(f"{_hour}:{_minute}", {})
+
+	highlights[str(day)][f"{_hour}:{_minute}"]["name"] = _name
+	highlights[str(day)][f"{_hour}:{_minute}"]["color"] = _color
+
+	writeJson(highlights, highlightPath)
+
+
+def getEntryData():
+	_hour, _minute = getTime()
+	_name = getString()
+	_color = getColor()
+
+	return _hour, _minute, _name, _color
+
+
+def createMissingPathObjects(SAVE_DIRECTORY, year, highlightPath):
+	# create missing dir
+	if not os.path.exists(os.path.join(SAVE_DIRECTORY, str(year))):
+		os.makedirs(os.path.join(SAVE_DIRECTORY, str(year)))
+
+	# create missing file
+	if not os.path.isfile(os.path.join(SAVE_DIRECTORY, highlightPath)):
+		writeJson({}, highlightPath)
